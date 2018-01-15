127330ec2104d20fbf63453ddf69f6727c7b6b0e
Adam Bekuretsion <abekuretsion@box.com>
2018-01-14 23:25:11 -0800

    initial commit

---
+++ src/components/AnotherCounter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="asyncIncrement({by: 50, duration: 500})">Increment</button>
+        <button class="btn btn-primary" @click="asyncDecrement({by: 50, duration: 500})">Decrement</button>
+    </div>
+</template>
+
+<script>
+    import * as types from '../store/types';
+    import { mapActions } from 'vuex';
+
+    export default {
+        methods: {
+            ...mapActions({
+                asyncIncrement: types.COUNTER_INCREMENT_ASYNC,
+                asyncDecrement: types.COUNTER_DECREMENT_ASYNC
+            })
+        }
+    }
+</script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="increment(100)">Increment</button>
+        <button class="btn btn-primary" @click="decrement(50)">Decrement</button>
+    </div>
+</template>
+
+<script>
+    import * as types from '../store/types';
+    import { mapActions } from 'vuex';
+
+    export default {
+        methods: {
+            ...mapActions({
+                increment: types.COUNTER_INCREMENT,
+                decrement: types.COUNTER_DECREMENT
+            })
+        }
+    }
+</script>
